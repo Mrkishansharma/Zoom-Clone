f3639f867470262ba1c3dc4e60679aec87ea524c
Mrkishansharma <kishansharma6377@gmail.com>
2023-02-27 19:20:11 +0530

    room id fix

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid")
 
-
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
 
 app.get('/',(req,res)=>{
     // res.status(200).send("Hello World")
